9c8607614a125845a4b50f3ab4ac363f291b7720
velor2012 <38395332+velor2012@users.noreply.github.com>
2023-03-10 15:25:44 +0800

    Update index.vue

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -5,19 +5,8 @@
           >Copyright&nbsp;&copy;&nbsp;{{ fullYear }}
           <a href="https://cwy666.com">DDD</a>
         </span>
-        <!-- 以下信息请不要修改哦 -->
-        <span class="hidden"
-          >&nbsp;&amp;&nbsp;Made&nbsp;by&nbsp;<a
-            :href="config.github"
-            target="_blank"
-          >
-            {{ config.author }}
-          </a></span
-        >&nbsp;&amp;
         <!-- 站点备案 -->
-        <a href="https://beian.miit.gov.cn" target="_blank"
-          >桂ICP备2021003681号</a
-        >
+        <a href="http://beian.miit.gov.cn/" target="_blank"><img src="https://img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png" width="20px" height="20px">桂ICP备2021003681号</a>
       </div>
       <div class="lrc" v-show="store.playerState">
         <music-one theme="filled" size="18" fill="#efefef" />
