e98a6e2d0be555fbc206bc2d6117c538b17c02b5
velor2012 <38395332+velor2012@users.noreply.github.com>
2025-04-09 11:30:23 +0800

    fix 字体

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -71,7 +71,7 @@ const siteIcp = ref(import.meta.env.VITE_SITE_ICP);
 const siteMoeIcp = ref(import.meta.env.VITE_SITE_MOE_ICP);
 const siteGABeiAnText = ref(import.meta.env.VITE_SITE_GA_BEIAN_TEXT);
 const siteGABeiAnCode = ref(import.meta.env.VITE_SITE_GA_BEIAN_CODE);
-const siteAnthor = ref(import.meta.env.VITE_SITE_ANTHOR);
+const siteAnthor = ref(import.meta.env.VITE_SITE_AUTHOR);
 const siteUrl = computed(() => {
   const url = import.meta.env.VITE_SITE_URL;
   if (!url) return "https://www.imsyy.top";
